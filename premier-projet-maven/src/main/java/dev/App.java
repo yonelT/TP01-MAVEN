59b91d69d3c97b56d00969269c492ce73bef23f2
Yonel TOUSSAY <yonel.toussay@gmail.com>
2019-04-12 15:23:43 +0200

    A la fin du TP02

--- premier-projet-maven/src/main/java/dev/App.java
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -1,14 +1,14 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-
-}
+public class App {
+	  public static void main(String[] args) {
+	    String asciiArt = FigletFont.convertOneLine("Salut Yonel");
+	    System.out.println(asciiArt);
+	  }
+	}
