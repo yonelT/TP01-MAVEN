47da283eff56ac6479f51c387c914e2d89ea477a
Yonel TOUSSAY <yonel.toussay@gmail.com>
2019-04-12 16:20:14 +0200

    A la fin du TP03

--- premier-projet-maven/src/main/java/dev/App.java
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -8,7 +9,15 @@ import com.github.lalyos.jfiglet.FigletFont;
  */
 public class App {
 	  public static void main(String[] args) {
-	    String asciiArt = FigletFont.convertOneLine("Salut Yonel");
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+	    String asciiArt = FigletFont.convertOneLine(titre);
 	    System.out.println(asciiArt);
+	    
+	    //affichage de l'environnement
+	    String environnement = ResourceBundle.getBundle("application").getString(
+	    "environnement");
+	    System.out.println("Environnement : " + environnement);
+	    
 	  }
-	}
+}
+
